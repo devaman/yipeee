e5e779b0c04569d97f4d585858688077dd4606b4
Amit Chambial <chambialamit@gmail.com>
2019-05-15 15:04:44 +0530

    Added Theme professional

---
+++ precache-manifest.b172e401e35b210ae3d049fb827232bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6abae582bb3c3a348fba",
+    "url": "/static/css/main.0a891ce6.chunk.css"
+  },
+  {
+    "revision": "6abae582bb3c3a348fba",
+    "url": "/static/js/main.6abae582.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "68cf7953afc73596ae1e",
+    "url": "/static/js/2.68cf7953.chunk.js"
+  },
+  {
+    "revision": "ce2d056cc0763b17633e7afd0d8d20c3",
+    "url": "/index.html"
+  }
+];
